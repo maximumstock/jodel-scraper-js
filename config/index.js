560abcfd77a152cd1e9d7974fb526eeafac77f9c
Maximilian Stock <me@maximumstock.net>
2017-08-21 22:59:17 +0200

    update client version to 4.57.1

--- config/index.js
+++ config/index.js
@@ -14,7 +14,7 @@ const default_config = {
     JODEL_API_V3: 'https://api.go-tellm.com/api/v3',
 
 		// Android client version of Jodel, see Google PlayStore
-    CLIENT_VERSION: '4.55.1',
+    CLIENT_VERSION: '4.57.1',
 		// static ID that is identical for all Android client versions
 		// (see various implementations on GitHub)
     CLIENT_ID: '81e8a76e-1e02-4d17-9ba0-8a7020261b26',
