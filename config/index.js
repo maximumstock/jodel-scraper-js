0d61cbd4ec6ade8f6e7faa8777ab6e2455018a45
Maximilian Stock <me@maximumstock.net>
2017-09-07 14:49:41 +0200

    remove unnecessary configuration files

--- config/index.js
+++ config/index.js
@@ -1,46 +1,18 @@
 /**
- * @file This module constructs and exports a configuration object based on
- * the default configuration below and the environment-specific configuration
- * that can be specified via the environment variable `NODE_ENV`.
- * `NODE_ENV` can be `development`, which is the default, `staging` or
- * `production`
+ * @file A central configuration object
  */
 
-// the default configuration
-const default_config = {
-
-    JODEL_API_HOST: 'https://api.go-tellm.com',
-    JODEL_API_V2: 'https://api.go-tellm.com/api/v2',
-    JODEL_API_V3: 'https://api.go-tellm.com/api/v3',
-
-		// Android client version of Jodel, see Google PlayStore
-    CLIENT_VERSION: '4.59.1',
-		// static ID that is identical for all Android client versions
-		// (see various implementations on GitHub)
-    CLIENT_ID: '81e8a76e-1e02-4d17-9ba0-8a7020261b26',
-    // signing key for HMAC generation for this client version
-    SECRET: 'mpOuBBukdOtDfFFOOxivviwYRTBoKiRRUayxYTHh',
-    // apparently 100 is the maximum number of Jodels one can receive per request
-    MAX_JODELS_PER_REQUEST: 100
-
-};
-
-function get_environment_config(env) {
-	switch(env) {
-		case 'development':	return require('./env/development.js');
-		case 'staging':			return require('./env/staging.js');
-		case 'production':	return require('./env/production.js')
-		default:						return require('./env/development.js')
-	}
-}
-
-// load configurations
-let config = default_config;
-const environment_config = get_environment_config(process.env.NODE_ENV);
-
-// overwrite default configuration with environment-specific settings
-Object.keys(environment_config).forEach(key => {
-	config[key] = environment_config[key];
-});
-
-module.exports = config;
+module.exports = {
+  
+      JODEL_API_HOST: 'https://api.go-tellm.com',
+      JODEL_API_V2: 'https://api.go-tellm.com/api/v2',
+      JODEL_API_V3: 'https://api.go-tellm.com/api/v3',
+  
+      // Android client version of Jodel, see Google PlayStore
+      CLIENT_VERSION: '4.59.1',
+      // static ID that is identical for all Android client versions
+      // (see various implementations on GitHub)
+      CLIENT_ID: '81e8a76e-1e02-4d17-9ba0-8a7020261b26',
+      // signing key for HMAC generation for this client version
+      SECRET: 'mpOuBBukdOtDfFFOOxivviwYRTBoKiRRUayxYTHh'
+  };
