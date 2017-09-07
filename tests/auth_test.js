45c8d9a30743111582a8a01d052149229e140cb2
Maximilian Stock <me@maximumstock.net>
2017-09-07 14:49:22 +0200

    update auth test

--- tests/auth_test.js
+++ tests/auth_test.js
@@ -13,7 +13,12 @@ const device_uid = 'ebed57121e34e6979b6031d332ddaa658e99e2fb6c199a27113fcee96b12
 
 api.request_token(device_uid, loc.latitude, loc.longitude)
   .then(response => {
-    console.log(response);
+    if(response.statusCode === 200) {
+      console.log('Yay, authentication still works!')
+    } else {
+      console.log('Authentication does not work anymore');
+      console.log(response.body, response.statusCode);
+    }
   })
   .catch(error => {
     console.error(error);
