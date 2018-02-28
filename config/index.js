4cdcf263ca79a6b8232890532def068ee894b3c6
Maximilian Stock <me@maximumstock.net>
2018-02-28 13:43:34 +0100

    Update Jodel HMAC signing secret and bump API client version

--- config/index.js
+++ config/index.js
@@ -9,10 +9,10 @@ module.exports = {
       JODEL_API_V3: 'https://api.go-tellm.com/api/v3',
   
       // Android client version of Jodel, see Google PlayStore
-      CLIENT_VERSION: '4.63.0',
+      CLIENT_VERSION: '4.77.4',
       // static ID that is identical for all Android client versions
       // (see various implementations on GitHub)
       CLIENT_ID: '81e8a76e-1e02-4d17-9ba0-8a7020261b26',
       // signing key for HMAC generation for this client version
-      SECRET: 'kHOqgoYZfmydKOzpWrIKfaUTmiCQuydZrSigayvB'
+      SECRET: 'pHiJcONSIdWTndQvUpyLadmCGbrkmPcKAlIEYokE'
   };
